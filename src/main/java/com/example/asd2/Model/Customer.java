aa24434f2d4b81768ebcb22520da4925fbdc9fd2
padishahMinecraftGamer <suhawken@gmail.com>
2024-09-06 17:22:16 +1000

    removed commented MongoDB dependencies

--- src/main/java/com/example/asd2/Model/Customer.java
+++ src/main/java/com/example/asd2/Model/Customer.java
@@ -1,12 +1,12 @@
-package com.example.asd2.Model;
-
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-@Document(collection = "users")
-public class Customer {
-    @Id
-    private String id;
-    private String username;
-    private String password;
-    private String role;
-}
+//package com.example.asd2.Model;
+//
+//import org.springframework.data.annotation.Id;
+//import org.springframework.data.mongodb.core.mapping.Document;
+//@Document(collection = "users")
+//public class Customer {
+//    @Id
+//    private String id;
+//    private String username;
+//    private String password;
+//    private String role;
+//}
