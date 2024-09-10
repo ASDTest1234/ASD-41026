abfe7aa853d58368348ab4d2f54a748dbae7dc80
AntonOlt <160560699+AntonOlt@users.noreply.github.com>
2024-09-10 15:46:20 +1000

    Add files via upload

--- src/main/java/com/example/asd2/repository/UserRepository.java
+++ src/main/java/com/example/asd2/repository/UserRepository.java
@@ -1,17 +1,8 @@
 package com.example.asd2.repository;
 
-
-import com.example.asd2.Model.Users;
+import com.example.asd2.model.Users;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-import org.springframework.security.core.userdetails.User;
 
-public interface UserRepository extends MongoRepository<Users, String> { // to interact with the mongoDB
-
-
+public interface UserRepository extends MongoRepository<Users, String> {
     Users findByEmail(String email);
-
-    Users findByPassword(String password);
-
-    Users findByRole(String role);
 }
