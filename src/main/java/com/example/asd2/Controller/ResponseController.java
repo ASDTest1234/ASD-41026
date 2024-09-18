e93bb04e42f9323c148e64ed4b90db8a2e093018
MinhTrinhHoang1 <MinhHoang.Trinh@student.uts.edu.au>
2024-09-18 19:17:48 +1000

    12

---
+++ src/main/java/com/example/asd2/Controller/ResponseController.java
@@ -0,0 +1,26 @@
+package com.example.asd2.Controller;
+
+import com.example.asd2.Model.Response;
+import com.example.asd2.service.ResponseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/tickets")
+public class ResponseController {
+
+    @Autowired
+    private ResponseService responseService;
+
+    @PostMapping
+    public ResponseEntity<Response> createResponse(@RequestBody Map<String, String> payload){
+        return new ResponseEntity<Response>(responseService.createResponse(payload.get("responseBody"), payload.get("ticket_id")), HttpStatus.CREATED);
+    }
+}
