609c487790d9f8d391b188eb8f2c0d6777a868ac
AntonOlt <160560699+AntonOlt@users.noreply.github.com>
2024-10-07 22:59:07 +1100

    Update Asd2Application.java

--- src/main/java/com/example/asd2/Asd2Application.java
+++ src/main/java/com/example/asd2/Asd2Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Asd2Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Asd2Application.class, args);
     }
-
 }
