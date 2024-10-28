89c8e2c9cdf1f620e90e8b3ecfcdd556d7e141a8
padishahMinecraftGamer <suhawken@gmail.com>
2024-10-29 10:31:07 +1100

    fixed ambiguous naming in user controller

--- src/main/java/com/example/asd2/Controller/UserController.java
+++ src/main/java/com/example/asd2/Controller/UserController.java
@@ -5,6 +5,8 @@ import com.example.asd2.Service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,7 +15,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/user/userDetails")
 public class UserController {
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
